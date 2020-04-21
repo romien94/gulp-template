72c92b53e8377c6b92053218d6fa2b1f02b77209
Roman <romanuskov94@gmail.com>
2020-04-21 14:40:37 +0300

    smooth scroll script refactored + safari support added

--- build/js/main.js
+++ build/js/main.js
@@ -49,6 +49,7 @@ function closeModalWindow() {
 }
 
 // window scroll
+// window scroll
 var scrollButton = document.querySelector('.scroll-button');
 var aboutSection = document.querySelector('#about');
 var consultButton = document.querySelector('.consult-button');
@@ -56,17 +57,44 @@ var feedbackSection = document.querySelector('#form');
 
 scrollButton.addEventListener('click', function (e) {
   e.preventDefault();
-  var aboutSectionOffsetTop = aboutSection.offsetTop;
-  var diff = aboutSectionOffsetTop - window.scrollY;
-  window.scrollBy(0, diff, {behavior: 'smooth'});
+  smoothScroll('#about', 1000, 50);
 });
 
 consultButton.addEventListener('click', function (e) {
   e.preventDefault();
-  var feedbackSectionOffsetTop = feedbackSection.offsetTop;
-  var diff = feedbackSectionOffsetTop - window.scrollY;
-  window.scrollBy(0, diff, {behavior: 'smooth'});
+  smoothScroll('#form', 1000);
 });
+
+function smoothScroll(target, duration, topPadding = 0) {
+  var target = document.querySelector(target);
+  var targetPosition = target.offsetTop;
+  var startPosition = window.scrollY;
+  var distance = targetPosition - startPosition + topPadding;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration)
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  function ease(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) {
+      return (c / 2 * t * t + b);
+    }
+    t--;
+    return -c / 2 * (t*(t-2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
 
 
 // phonemask check
